96294125fce4172d721f005e972a4c35380ba247
Mark Gerrior <github@gerrior.com>
2020-05-26 16:07:52 -0700

    Initial project and gitignore

---
+++ Editor Review Tones/Editor Review TonesTests/Editor_Review_TonesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Editor_Review_TonesTests.swift
+//  Editor Review TonesTests
+//
+//  Created by Mark Gerrior on 5/26/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import XCTest
+@testable import Editor_Review_Tones
+
+class Editor_Review_TonesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
